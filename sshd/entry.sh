9ed373970de5bd8e405f5a72b3e613d5fc32ee55
姜璐 <lu.jiang@kalendsoft.com>
2020-04-16 17:34:24 +0800

    enable sudo

--- sshd/entry.sh
+++ sshd/entry.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 # echo "Your Passwd is: ${PASSWD}"
 
 echo "root:${PASSWD}" | chpasswd
@@ -9,7 +7,6 @@ echo "root:${PASSWD}" | chpasswd
 echo "add user ${USER2} with passwd  ${PASSWD2}"
 useradd -r -m -p '' -u 1000 --shell /bin/bash  $USER2
 echo "${USER2}:${PASSWD2}" | chpasswd
-#usermod -aG sudo $USER2
-echo "${USER2} ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+echo "${USER2}   ALL=(ALL)       ALL" > /etc/sudoers
 
 exec "/usr/sbin/sshd" "-D"
